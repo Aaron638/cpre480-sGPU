a664ef4a66eeffe54ff2d5ea88a7dbda11cb1fc0
Bryce Snell <paecific.jr@gmail.com>
2021-02-21 16:56:22 -0600

    Added an axi_transmit function, first test

--- MP-1/hw/ip/sgp_viewPort/src/sgp_viewPort_tb_test.tcl
+++ MP-1/hw/ip/sgp_viewPort/src/sgp_viewPort_tb_test.tcl
@@ -1,18 +1,17 @@
+# Helper functions
+
+proc axi_transmit {start_time {length "10ns"} data} {
+    valid_pulse_time = "1ns"
+    add_force {/sgp_viewPort_tb/S_AXIS_TVALID} -radix hex {1 $start_time} -cancel_after $valid_pulse_time
+    add_force {/sgp_viewPort_tb/S_AXIS_TDATA} -radix hex {$data $start_time} -cancel_after $length
+}
+
 # Start simulation
 launch_simulation
-source sgp_viewPort_tb.tcl 
 current_wave_config {sgp_viewPort_tb_waves}
 
-# Add waves
-add_wave {{/sgp_viewPort_tb/S_AXIS_TREADY}} 
-add_wave {{/sgp_viewPort_tb/S_AXIS_TDATA}} 
-add_wave {{/sgp_viewPort_tb/S_AXIS_TLAST}} 
-add_wave {{/sgp_viewPort_tb/S_AXIS_TVALID}} 
-
 # Test axi signals
-add_force {/sgp_viewPort_tb/S_AXIS_TVALID} -radix hex {0 0ns} -cancel_after 5ps
-add_force {/sgp_viewPort_tb/S_AXIS_TDATA} -radix hex {1 5ps} -cancel_after 10ps
-add_force {/sgp_viewPort_tb/S_AXIS_TVALID} -radix hex {1 5ps} -cancel_after 10ps
+axi_transmit "5ps" "DEADBEEF"
 
 # Run the simulation
 run all
