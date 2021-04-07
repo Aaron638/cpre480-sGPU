99eb4648a0af4b40b60de6b6cea598a52fe4c071
Bryce Snell <paecific.jr@gmail.com>
2021-04-07 12:48:54 -0500

    Added the parts needed to write to the depth buffer

--- SGP/hw/ip/sgp_renderOutput/src/sgp_renderOutput.vhd
+++ SGP/hw/ip/sgp_renderOutput/src/sgp_renderOutput.vhd
@@ -202,10 +202,10 @@ architecture behavioral of sgp_renderOutput is
   end component dcache;
 
 
-  type STATE_TYPE is (WAIT_FOR_FRAGMENT, CHECK_DEPTH, GEN_ADDRESS, WRITE_ADDRESS, WAIT_FOR_RESPONSE);
+  type STATE_TYPE is (WAIT_FOR_FRAGMENT, CHECK_DEPTH, GEN_ADDRESS_COLOR, GEN_ADDRESS_DEPTH, WRITE_ADDRESS, WAIT_FOR_RESPONSE);
   signal state        : STATE_TYPE;
 
-  type DEPTH_BUFFER is array(1920 downto 0) of array(1080 downto 0) of fixed_t;  -- TODO make sure this a) is the right size, b) synthesizes
+  type DEPTH_BUFFER is array(1920 downto 0) of array(1080 downto 0) of fixed_t;  -- TODO use the buffer in the fancy memory
   signal depth_buffer : DEPTH_BUFFER;
    
 
@@ -439,18 +439,44 @@ begin
             -- If it isn't, then skip to the next fragment
             -- If it is, proceed to write
             when CHECK_DEPTH =>
+                -- renderoutput_depthbuffer < here is your base address
                 if (z_pos_fixed > depth_buffer(x_pos_fixed)(y_pos_fixed)) then
-                    state <= GEN_ADDRESS;
+                    state <= GEN_ADDRESS_DEPTH;
                 else
                     state <= WAIT_FOR_FRAGMENT;
                 end if;
 
+            when GEN_ADDRESS_DEPTH => 
+                -- xvp and yvp are Q16.16s that need to be converted into signed ints
+                -- Round if fraction >= 0.5
+                if (x_pos_fixed(15) = '1') then
+                    x_pos_short_reg <= x_pos_fixed(31 downto 16) + 1;
+                else
+                    x_pos_short_reg <= x_pos_fixed(31 downto 16);
+                end if;
+                
+                if (y_pos_fixed(15) = '1') then
+                    y_pos_short_reg <= y_pos_fixed(31 downto 16) + 1;
+                else
+                    y_pos_short_reg <= y_pos_fixed(31 downto 16);
+                end if;
+
+                if (z_pos_fixed(15) = '1') then
+                    z_pos_short_reg <= z_pos_fixed(31 downto 16) + 1;
+                else
+                    z_pos_short_reg <= z_pos_fixed(31 downto 16);
+                end if;
+
+                frag_address    <= signed(renderoutput_depthbuffer) + (7680 * (1080 - y_pos_short_reg)) + (x_pos_short_reg * 4) ;    
+                frag_color      <= std_logic_vector(z_pos_fixed);  -- We can actually store the full 16.16 since we have 32 bits just for this value
+            
+                state <= GEN_ADDRESS_COLOR;
             -- To generate the address value, we have to get info from the global framebuffer memory
             -- sgp_graphics.c has the addressses for the 2 color buffers we use, we want to always draw to the "back buffer"
             -- SGP_graphicsmap[SGP_RENDER_OUTPUT] contains the render output config
             -- The config will tell us whether COLORBUFFER_1 or 2 is the backbuffer
             -- sgp_graphics.h defines SGP_AXI_RENDEROUTPUT_COLORBUFFER as the renderOutput register 0x0000
-            when GEN_ADDRESS =>
+            when GEN_ADDRESS_COLOR =>
                 -- xvp and yvp are Q16.16s that need to be converted into signed ints
                 -- Round if fraction >= 0.5
                 if (x_pos_fixed(15) = '1') then
