1ae00b6eb627c065f84fe97b3b8826d3ea26dd2a
jkfrench <jkfrench@iastate.edu>
2021-04-12 13:56:48 -0500

    fix array mismatch error

--- SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/alphaBlending_behav/webtalk/xsim_webtalk.tcl
+++ SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/alphaBlending_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/sgp_renderOutput_behav/webtalk/
+webtalk_init -webtalk_dir U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/alphaBlending_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Apr 12 12:04:19 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Apr 12 13:13:45 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2902540" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -22,11 +22,21 @@ webtalk_add_data -client project -key cpu_speed -value "3696 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
-webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
-webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7664_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2430938515 -regid "174156856_174156857_210694690_929" -xml U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/sgp_renderOutput_behav/webtalk/usage_statistics_ext_xsim.xml -html U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/sgp_renderOutput_behav/webtalk/usage_statistics_ext_xsim.html -wdm U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/sgp_renderOutput_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key File_Counter -value "8" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key Command -value "xelab" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key Vhdl2008 -value "false" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key GenDLL -value "false" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key SDFModeling -value "false" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key HWCosim -value "false" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key DPI_Used -value "false" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key Debug -value "typical" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key Simulation_Image_Code -value "80 KB" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Image_Data -value "3 KB" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Total_Processes -value "10" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Total_Instances -value "6" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip " -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Time -value "0.92_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "65012_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1306732261 -regid "174156856_174156857_210694690_929" -xml U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/alphaBlending_behav/webtalk/usage_statistics_ext_xsim.xml -html U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/alphaBlending_behav/webtalk/usage_statistics_ext_xsim.html -wdm U:/cpre480/SGP/hw/ip/sgp_renderOutput/proj/sgp_renderOutput.sim/sim_1/behav/xsim/xsim.dir/alphaBlending_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- SGP/hw/ip/sgp_renderOutput/src/alphaBlending.vhd
+++ SGP/hw/ip/sgp_renderOutput/src/alphaBlending.vhd
@@ -6,7 +6,7 @@ use WORK.sgp_types.all;
 entity alphaBlending is 
 
 	port( 
-		glEnable          : in std_logic;
+		glEnable          : in std_logic; --not used as of right now
 		a_src_color       : in fixed_t; --needs to come in as the Q16.16 form before it is multiplied by 255
 		r_src_color       : in fixed_t;
 		b_src_color       : in fixed_t;
@@ -19,8 +19,8 @@ entity alphaBlending is
 		r_blend_color     : out fixed_t;
 		b_blend_color : out fixed_t;
 		g_blend_color : out fixed_t;
-		src_factor 	: in std_logic_vector(4 downto 0);
-		dst_factor  : in std_logic_vector(4 downto 0));
+		src_factor 	: in std_logic_vector(3 downto 0);
+		dst_factor  : in std_logic_vector(3 downto 0));
 		
 end alphaBlending;
 
@@ -51,14 +51,25 @@ architecture arc of alphaBlending is
 	constant GL_DST_ALPHA        			: std_logic_vector(3 downto 0) := "1000";
 	constant GL_ONE_MINUS_DST_ALPHA 		: std_logic_vector(3 downto 0) := "1001";
 	--not required to implement
-	constant GL_CONSTANT_COLOR      		: std_logic_vector(3 downto 0) := "0000";
-	constant GL_ONE_MINUS_CONSTANT_COLOR    : std_logic_vector(3 downto 0) := "0000";
-	constant GL_CONSTANT_ALPHA        		: std_logic_vector(3 downto 0) := "0000";
-	constant GL_ONE_MINUS_CONSTANT_ALPHA    : std_logic_vector(3 downto 0) := "0000";
+	--constant GL_CONSTANT_COLOR      		: std_logic_vector(3 downto 0) := "0000";
+	--constant GL_ONE_MINUS_CONSTANT_COLOR    : std_logic_vector(3 downto 0) := "0000";
+	--constant GL_CONSTANT_ALPHA        		: std_logic_vector(3 downto 0) := "0000";
+	--constant GL_ONE_MINUS_CONSTANT_ALPHA    : std_logic_vector(3 downto 0) := "0000";
 	--this may still be necessary
-	constant GL_SRC_ALPHA_SATURATE        	: std_logic_vector(3 downto 0) := "0000";
+	--constant GL_SRC_ALPHA_SATURATE        	: std_logic_vector(3 downto 0) := "0000";
 
 begin
+    --needs to be truncated down to be Q16.16
+    a_temp <= a_src * a_src_color + a_dst * a_dst_color;
+    r_temp <= r_src * r_src_color + r_dst * r_dst_color;
+    b_temp <= b_src * b_src_color + b_dst * b_dst_color;
+    g_temp <= g_src * g_src_color + g_dst * g_dst_color;
+    
+	a_blend_color <= a_temp(47 downto 16); 
+	r_blend_color <= r_temp(47 downto 16);
+	b_blend_color <= b_temp(47 downto 16);
+	g_blend_color <= g_temp(47 downto 16);
+
 process(src_factor) begin
 	case src_factor is 
 		when GL_ZERO => a_src <= fixed_t_zero;
@@ -102,12 +113,12 @@ process(src_factor) begin
 										b_src <= fixed_t_one - a_dst_color;
 										g_src <= fixed_t_one - a_dst_color;
 		--not required to implement								
-		when GL_CONSTANT_COLOR => 
-		when GL_ONE_MINUS_CONSTANT_COLOR => 
-		when GL_CONSTANT_ALPHA => 
-		when GL_ONE_MINUS_CONSTANT_ALPHA => 
+		--when GL_CONSTANT_COLOR => 
+		--when GL_ONE_MINUS_CONSTANT_COLOR => 
+		--when GL_CONSTANT_ALPHA => 
+		--when GL_ONE_MINUS_CONSTANT_ALPHA => 
 		--this may still be necessary
-		when GL_SRC_ALPHA_SATURATE => 
+		--when GL_SRC_ALPHA_SATURATE => 
 		when others => 
 	end case;
 end process;
@@ -155,25 +166,13 @@ process (dst_factor) begin
 										b_dst <= fixed_t_one - a_dst_color;
 										g_dst <= fixed_t_one - a_dst_color;
 		--not required to implement
-		when GL_CONSTANT_COLOR => 
-		when GL_ONE_MINUS_CONSTANT_COLOR => 
-		when GL_CONSTANT_ALPHA => 
-		when GL_ONE_MINUS_CONSTANT_ALPHA => 
+		--when GL_CONSTANT_COLOR => 
+		--when GL_ONE_MINUS_CONSTANT_COLOR => 
+		--when GL_CONSTANT_ALPHA => 
+		--when GL_ONE_MINUS_CONSTANT_ALPHA => 
 		--this may still be necessary
-		when GL_SRC_ALPHA_SATURATE => 
+		--when GL_SRC_ALPHA_SATURATE => 
 		when others =>
 	end case;
-end process;
-
-    --needs to be truncated down to be Q16.16
-    a_temp <= a_src * a_src_color + a_dst * a_dst_color;
-    r_temp <= r_src * r_src_color + r_dst * r_dst_color;
-    b_temp <= b_src * b_src_color + b_dst * b_dst_color;
-    g_temp <= g_src * g_src_color + g_dst * g_dst_color;
-    
-	a_blend_color <= a_temp(47 downto 16); 
-	r_blend_color <= r_temp(47 downto 16);
-	b_blend_color <= b_temp(47 downto 16);
-	g_blend_color <= g_temp(47 downto 16);
-	
+end process;	
 end architecture arc;
